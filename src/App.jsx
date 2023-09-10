b559ebc1a94f884efdf4c9f4cbd3b3407e05180c
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2023-09-10 11:46:24 +0530

    Dark mode changes

--- src/App.jsx
+++ src/App.jsx
@@ -20,20 +20,20 @@ const App = () => {
     }, 2000);
   }, []);
   return !isLoading ? (
-    <div className="bg-primary w-full overflow-hidden">
+    <div className="bg-primary dark:bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
           <Navbar />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.flexStart}`}>
+      <div className={`bg-primary dark:bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Hero />
         </div>
       </div>
 
-      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+      <div className={`bg-primary dark:bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
           <Business />
           <Timeline />
@@ -45,7 +45,7 @@ const App = () => {
       </div>
     </div>
   ) : (
-    <div className="bg-primary flex flex-col justify-center items-center w-full h-screen overflow-hidden">
+    <div className="bg-primary dark:bg-primary flex flex-col justify-center items-center w-full h-screen overflow-hidden">
       <img src={mylogo} alt="loader" className="animate-pulse w-[96px] md:w-[136px] mb-8" />
     </div>
   );
