c30103c994d2607d69f78063eb27dfc2fbdf30eb
Sourabh Sikarwar <sourabhsikarwar1812@gmail.com>
2024-01-22 12:38:11 +0530

    Dynamic year in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,53 +2,58 @@ import { mylogo } from "../assets";
 import { navLinks, socialMedia } from "../constants";
 import { motion } from "framer-motion";
 
-const Footer = () => (
-  <footer className="bg-primary dark:bg-primary relative">
-    <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 z-100">
-      <div className="flex justify-center"
-      initial={{ scale: 0.6, opacity: 0 }}
-      whileInView={{ scale: 1, opacity: 1 }}
-      transition={{ duration: 0.5 }}
-      >
-        <img src={mylogo} alt="logo" className="w-40 shadow-img" />
-      </div>
+const Footer = () => {
+  const year = new Date().getFullYear();
 
-      <ul className="z-100 list-none my-12 flex sm:flex-row gap-4 sm:gap-8 flex-col justify-center items-center flex-1">
-        {navLinks.map((nav, index) => (
-          <li
-            key={nav.id}
-            className="font-poppins font-normal cursor-pointer text-[14px] hover:text-gray-300 text-white dark:text-white transition-all duration-200"
-          >
-            <a href={`#${nav.id}`}>{nav.title}</a>
-          </li>
-        ))}
-      </ul>
+  return (
+    <footer className="bg-primary dark:bg-primary relative">
+      <div className="mx-auto max-w-7xl px-4 py-16 sm:px-6 lg:px-8 z-100">
+        <div
+          className="flex justify-center"
+          initial={{ scale: 0.6, opacity: 0 }}
+          whileInView={{ scale: 1, opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          <img src={mylogo} alt="logo" className="w-40 shadow-img" />
+        </div>
 
-      <div className="flex justify-center gap-8">
-        {socialMedia.map((social, index) => (
-          <a
-            href={social.link}
-            key={index}
-            className="text-white dark:text-white hover:text-dimWhite transition-all duration-200"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
+        <ul className="z-100 list-none my-12 flex sm:flex-row gap-4 sm:gap-8 flex-col justify-center items-center flex-1">
+          {navLinks.map((nav, index) => (
+            <li
+              key={nav.id}
+              className="font-poppins font-normal cursor-pointer text-[14px] hover:text-gray-300 text-white dark:text-white transition-all duration-200"
             >
-              <path d={social.pathD} />
-            </svg>
-          </a>
-        ))}
+              <a href={`#${nav.id}`}>{nav.title}</a>
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex justify-center gap-8">
+          {socialMedia.map((social, index) => (
+            <a
+              href={social.link}
+              key={index}
+              className="text-white dark:text-white hover:text-dimWhite transition-all duration-200"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path d={social.pathD} />
+              </svg>
+            </a>
+          ))}
+        </div>
       </div>
-    </div>
-    <div className="mt-0 border-t border-dimBlue dark:border-dimBlue py-8 z-10">
-      <p className="text-center text-xs/relaxed text-gray-400 dark:text-gray-400">
-        © 2023. All rights reserved. Created by Sourabh Sikarwar
-      </p>
-    </div>
-  </footer>
-);
+      <div className="mt-0 border-t border-dimBlue dark:border-dimBlue py-8 z-10">
+        <p className="text-center text-xs/relaxed text-gray-400 dark:text-gray-400">
+          © {year}. All rights reserved. Created by Sourabh Sikarwar
+        </p>
+      </div>
+    </footer>
+  );
+};
 
 export default Footer;
